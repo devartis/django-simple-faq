d7aacdc905445f0b04843558dc51823be66f2684
lgomez <lgomez@devartis.com>
2013-03-15 16:06:20 -0300

    Test app !

---
+++ test_app/test_app/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'test_app.views.home', name='home'),
+    # url(r'^test_app/', include('test_app.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^faq/', include('simple_faq.urls')),
+)
