e5bec10f3788e435af63970158e35cb3f2ad4f2a
lgomez <lgomez@devartis.com>
2013-03-15 11:51:30 -0300

    Fix father class of topics view
    @15m

--- simple_faq/views.py
+++ simple_faq/views.py
@@ -1,8 +1,8 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 from simple_faq.models import Topic
 
 
-class Topics(TemplateView):
+class Topics(ListView):
     template_name = "topics.html"
     context_object_name = "topics"
     model = Topic
