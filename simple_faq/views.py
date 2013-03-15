fd844712310380bc839b02aaf158fac7d0755585
lgomez <lgomez@devartis.com>
2013-03-15 11:50:09 -0300

    Added view to all topics with their questions and answers
    @15m

--- simple_faq/views.py
+++ simple_faq/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.views.generic import TemplateView
+from simple_faq.models import Topic
+
+
+class Topics(TemplateView):
+    template_name = "topics.html"
+    context_object_name = "topics"
+    model = Topic
