843555a7ca2f7cd8a8617becfbd0f52a7b428977
lgomez <lgomez@devartis.com>
2013-03-15 12:13:03 -0300

    Addded templates and change Topics template
    @30m

--- simple_faq/views.py
+++ simple_faq/views.py
@@ -3,6 +3,6 @@ from simple_faq.models import Topic
 
 
 class Topics(ListView):
-    template_name = "topics.html"
+    template_name = "topic_list.html"
     context_object_name = "topics"
     model = Topic
