233dea182c1548f6036dffd05fb13f0376afddb3
lgomez <lgomez@devartis.com>
2013-03-18 11:48:04 -0300

    Fixed the media configuration to show images.

--- test_app/test_app/urls.py
+++ test_app/test_app/urls.py
@@ -1,19 +1,20 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'test_app.views.home', name='home'),
-    # url(r'^test_app/', include('test_app.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+urlpatterns = patterns(
+    '',
     url(r'^admin/', include(admin.site.urls)),
-
     url(r'^faq/', include('simple_faq.urls')),
 )
+
+urlpatterns += staticfiles_urlpatterns()
+if settings.DEBUG:
+    urlpatterns += patterns(
+        '',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    )
